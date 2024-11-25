b03fa501911906d6332b8add2044766d0b53f9fa
Jude Woolls <jwoolls01@gmail.com>
2024-11-25 12:56:03 +0000

    Add event listeners for the DOM loading and the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// event listeners
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+    for (let b of buttons) {
+        b.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('you clicked submit');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked: ${gameType}`);
+            }
+        })
+    }
+});
+
+
+// functions
 function runGame() {}
 function checkAnswer() {}
 function calculateCorrectAnswer() {}
