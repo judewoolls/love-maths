2d836769c61896c6a25cf704fa3630ca2cab8781
Jude Woolls <jwoolls01@gmail.com>
2024-11-25 13:11:21 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,40 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert('you clicked submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked: ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition')
 });
 
 
-// functions
-function runGame() {
+/**
+ * the main game loop called when the script is first loaded 
+ * and after users answer is processed
+ */
+function runGame(gameType) {
+
+    // creates random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert('Unknown Game Type');
+        throw `Unknown game type: ${gameType}. Aborting`;
+    }
 }
 
 function checkAnswer() {}
 function calculateCorrectAnswer() {}
 function incrementWrongAnswer() {}
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+}
 function displaySubtractQuestion() {}
 function displayMultiplyQuestion() {}
